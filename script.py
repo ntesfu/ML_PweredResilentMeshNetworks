e05dcf9f628c1e2475dee6ba02c4a2312f470e61
Eyaphet <eyaphi24@gmail.com>
2023-01-09 17:41:59 +0400

    configured the script to work with multiple nodes

--- script.py
+++ script.py
@@ -22,7 +22,7 @@ class datacollector():
         self.columns = 'systemTime,' + 'ipAddress,'+  'multiPointRelaySelector,' +'linkCost,' + 'linkQuality,' + 'neighborLinkQuality,' + 'RSSI value,' + 'AVG RSSI value,'+ 'Connected,\n'
         
 
-        self.file = None
+        self.file = dict()
         self.ssidreg = "KU-AP.*"
 
 #get the connected node ip
@@ -39,6 +39,12 @@ class datacollector():
         #wsl
         self.jsonaddress = 'http://192.168.10.1:9090/all'
 
+        self.macentry = dict()
+        self.macentry['10.79.141.204'] = '00:30:1a:4f:8d:cc'
+        self.macentry['10.79.141.55'] = '00:30:1a:4f:8d:37'
+        self.macentry['10.79.141.35'] = '00:30:1a:4f:8d:23'
+
+
 
     def __call__(self):
         self.get_entry()
@@ -53,51 +59,68 @@ class datacollector():
 
                 stationdata = self.node.run("iw dev wlp1s0 station dump | grep -E 'Station|signal'").stdout.splitlines()
                 #stationdata holds the station data and the signal values
+                rssi = dict()
+                avgrssi = dict()
 
-                rssi = int(stationdata[1][stationdata[1].index(':')+1:stationdata[1].index('[')].strip()) # Regex
-                avgrssi = int(stationdata[2][stationdata[2].index(':')+1:stationdata[2].index('[')].strip()) # Regex
+                for i in range(0,len(stationdata),3):
+                    #get the mac address
+                    mac = stationdata[i][stationdata[i].index('n')+1:stationdata[i].index('(')].strip()
+                    print(mac)
+
+
+                    rssi[mac] = int(stationdata[i+1][stationdata[i+1].index(':')+1:stationdata[i+1].index('[')].strip()) # Regex
+                    avgrssi[mac] = int(stationdata[i+2][stationdata[i+2].index(':')+1:stationdata[i+2].index('[')].strip()) # Regex
 
                 # we take both signal and average signal values
 
                 if (len(data['links'])!=0):
-
-                    dictdata = dict()  
-                    dictdata = {'Systemtime': str(data['systemTime']), 'ip':str(data['neighbors'][0]['ipAddress']), 'mprs':str(data['neighbors'][0]['multiPointRelaySelector']) \
-                    , 'linkcost': str(data['links'][0]['linkCost']) ,'LinkQuality':str(data['links'][0]['linkQuality']), 'NQ': str(data['links'][0]['neighborLinkQuality']),'RSSI': rssi, 'AVGRSSI': avgrssi}
-                    temp[str(data['neighbors'][0]['ipAddress'])] = dictdata
-                    self.to_file_connected(temp, 1)
+                    for i in data['links']:
+                        dictdata = dict()  
+                        dictdata = {'Systemtime': str(data['systemTime']), 'ip':str(i['remoteIP']), 'mprs':str(data['neighbors'][0]['multiPointRelaySelector']) \
+                        , 'linkcost': str(i['linkCost']) ,'LinkQuality':str(i['linkQuality']), 'NQ': str(i['neighborLinkQuality']),'RSSI': rssi[self.macentry[str(i['remoteIP'])]], 'AVGRSSI': avgrssi[self.macentry[str(i['remoteIP'])]]}
+                        temp[str(i['remoteIP'])] = dictdata
+                        self.to_file_connected(temp, 1,str(i['remoteIP']))
 
                 else:
+                    #just close the files
                     #means the node has been disconnected
-                    self.to_file_connected(temp, 0)
+                    #self.to_file_connected(temp, 0)
+                    for i in self.file:
+                        self.file[i].close()
 
                 timedelta = int(time.time()*1000.0) - t 
                 time.sleep(2-timedelta/1000.0)
                 print(2-timedelta/1000.0)
         except KeyboardInterrupt:
-            self.file.close()
+            for i in self.file:
+                self.file[i].close()
 
-    def to_file_connected(self,info,value):
+    def to_file_connected(self,info,value,ip):
         #if connection
         if (value == 1):
-            if self.file is None:
-                self.file = open(str(self.filename)+".csv", 'w')
-                self.file.write(self.columns)
-                self.file.close()
-                self.file = open(str(self.filename)+".csv", 'a')
+            if ip not in self.file:
+                self.filename +=1
+                self.file[ip] = open(str(self.filename)+".csv", 'w')
+                self.file[ip].write(self.columns)
+                self.file[ip].close()
+                self.file[ip] = open(str(self.filename)+".csv", 'a')
 
-
-            dataToWrite = info['Systemtime']+','+ info['ip'] + ','  + info['mprs'] + ',' + info['linkcost'] + ',' + info['LinkQuality'] + ',' + info['NQ']  + ',' + str(info['RSSI']) + ','  + str(info['AVGRSSI'])  + ',' + str(value) + '\n'
-            self.file.write(dataToWrite)
+            
+            dataToWrite = info[ip]['Systemtime']+','+ info[ip]['ip'] + ','  + info[ip]['mprs'] + ',' + info[ip]['linkcost'] + ',' + info[ip]['LinkQuality'] + ',' + info[ip]['NQ']  + ',' + str(info[ip]['RSSI']) + ','  + str(info[ip]['AVGRSSI'])  + ',' + str(value) + '\n'
+            self.file[ip].write(dataToWrite)
         #if no connection (disconnection or we havent started yet)
         else:
             #there is no link data/ the node just disconnected
+            dataToWrite = info['Systemtime']+','+ info['ip'] + ','  + info['mprs'] + ',' + info['linkcost'] + ',' + info['LinkQuality'] + ',' + info['NQ']  + ',' + str(info['RSSI']) + ','  + str(info['AVGRSSI'])  + ',' + str(value) + '\n'
+            self.file[ip].write(dataToWrite)
+            '''
             if self.file is not None:
                 dataToWrite = info['Systemtime']+','+ str(0) + ','  + info['mprs'] + ',' + info['linkcost'] + ',' + info['LinkQuality'] + ',' + info['NQ']  + ',' + str(info['RSSI']) + ','  + str(info['AVGRSSI'])  + ',' + str(value) + '\n'
                 self.file.write(dataToWrite)
                 self.file.close()
                 self.file = None
                 self.filename += 1
+                '''
 
 
 if __name__=="__main__":
